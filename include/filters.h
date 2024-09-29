a111f777f0b450c7eddb9a2407e401b1f45b92dd
hedge0 <eliasmemail@gmail.com>
2024-09-28 23:28:10 -0400

    added additional filter for bad calculations

--- include/filters.h
+++ include/filters.h
@@ -8,5 +8,6 @@
 double calculate_standard_deviation(const std::vector<double> &strikes);
 std::vector<double> filter_strikes(const std::vector<double> &strikes, double S, double num_stdev = 1.25, bool two_sigma_move = false);
 std::map<double, QuoteData> filter_by_bid_price(const std::map<double, QuoteData> &data);
+std::map<double, QuoteData> filter_by_mid_iv(const std::map<double, QuoteData> &data);
 
 #endif
