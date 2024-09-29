c6b46f1609f9781f8791fdacf6f0ac57950b101b
hedge0 <eliasmemail@gmail.com>
2024-09-28 23:09:05 -0400

    added new filtering and data set

---
+++ include/filters.h
@@ -0,0 +1,12 @@
+#ifndef FILTERS_H
+#define FILTERS_H
+
+#include <vector>
+#include <map>
+#include "data.h"
+
+double calculate_standard_deviation(const std::vector<double> &strikes);
+std::vector<double> filter_strikes(const std::vector<double> &strikes, double S, double num_stdev = 1.25, bool two_sigma_move = false);
+std::map<double, QuoteData> filter_by_bid_price(const std::map<double, QuoteData> &data);
+
+#endif
--- include/models.h
+++ include/models.h
@@ -5,4 +5,4 @@
 
 double calculate_implied_volatility_baw(double option_price, double S, double K, double r, double T, double q = 0.0, const std::string &option_type = "calls", int max_iterations = 100, double tolerance = 1e-8);
 
-#endif // MODELS_H
+#endif
