82312cc1044e5a5e4343e4d880cb1cb0f6c27ba3
Elias Martin <elias@sportsvisio.com>
2024-09-29 01:36:00 -0400

    added variables as globals in load env

--- include/load_env.h
+++ include/load_env.h
@@ -3,6 +3,12 @@
 
 #include <string>
 
+extern const char *schwab_api_key;
+extern const char *schwab_secret;
+extern const char *callback_url;
+extern const char *account_hash;
+extern const char *fred_api_key;
+
 void load_env_file(const std::string &file_path);
 
 #endif
