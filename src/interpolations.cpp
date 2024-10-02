306f43d9fb67448a28004b23347544ef0b5d44ba
Elias Martin <elias@sportsvisio.com>
2024-10-02 00:45:49 -0400

    added rfv

--- src/interpolations.cpp
+++ src/interpolations.cpp
@@ -1,6 +1,8 @@
 #include <Eigen/Dense>
 #include "rbf.h"
 #include "interpolations.h"
+#include <functional>
+#include <algorithm>
 
 /**
  * @brief Function for RBF Interpolation model.
@@ -25,4 +27,49 @@ std::function<Eigen::VectorXd(const Eigen::VectorXd &)> rbf_model(const Eigen::V
     {
         return rbf.interpolate(inputs);
     };
+}
+
+/**
+ * @brief RFV Model function for implied volatility fitting.
+ *
+ * @param k Log-moneyness of the option.
+ * @param params Parameters [a, b, c, d, e] for the RFV model.
+ * @return double The calculated RFV model value for the given log-moneyness.
+ */
+double rfv_model(double k, const Eigen::VectorXd &params)
+{
+    double a = params[0];
+    double b = params[1];
+    double c = params[2];
+    double d = params[3];
+    double e = params[4];
+
+    return (a + b * k + c * k * k) / (1 + d * k + e * k * k);
+}
+
+/**
+ * @brief Objective function to minimize during model fitting (WLS method).
+ *
+ * @param params Vector of model parameters.
+ * @param k Vector of log-moneyness.
+ * @param y_mid Vector of mid prices of the options.
+ * @param y_bid Vector of bid prices of the options.
+ * @param y_ask Vector of ask prices of the options.
+ * @return double The calculated objective value (WLS).
+ */
+double objective_function(const Eigen::VectorXd &params, const Eigen::VectorXd &k, const Eigen::VectorXd &y_mid, const Eigen::VectorXd &y_bid, const Eigen::VectorXd &y_ask)
+{
+    Eigen::VectorXd spread = y_ask - y_bid;
+    double epsilon = 1e-8;
+    Eigen::VectorXd weights = 1.0 / (spread.array() + epsilon);
+
+    Eigen::VectorXd residuals(k.size());
+    for (int i = 0; i < k.size(); ++i)
+    {
+        residuals[i] = rfv_model(k[i], params) - y_mid[i];
+    }
+
+    Eigen::VectorXd weighted_residuals = weights.array() * residuals.array().square();
+
+    return weighted_residuals.sum();
 }
